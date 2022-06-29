5883c016c36f692aca640c5d08496c16fbd707aa
Michael Clautice <michaelclauticeusa@gmail.com>
2022-06-29 19:19:04 -0400

    W.6.29.22.07:19p

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,14 @@
 // Michael Clautice, Richmond, VA
 import React from "react";
 import Tilt from "react-tilt";
+import './Logo.css';
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
 
       <Tilt
-        className="Tilt"
-        options={{ max: 25 }}
-        style={{ height: 150, width: 150 }}
+className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }}
       >
         <div className="Tilt-inner"> 🔴 </div>
       </Tilt>
